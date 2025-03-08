53d163da355abff88d81e186fe06cc44e3deb872
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2025-03-08 20:15:34 +0700

    Try to mess around

--- language/java/oop/Encapsulation/Latihan1/src/App.java
+++ language/java/oop/Encapsulation/Latihan1/src/App.java
@@ -4,6 +4,6 @@ class Player {
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hell!");
+        System.out.println("Hell w!");
     }
 }
