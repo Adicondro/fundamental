3f93279fec51812f95ae6ea9a2e6f3928d8abe14
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2025-03-12 21:11:34 +0700

    setting up express, golang for backend, and java oop mastering

--- language/java/oop/Encapsulation/Latihan1/src/App.java
+++ language/java/oop/Encapsulation/Latihan1/src/App.java
@@ -1,26 +1,64 @@
 class Player {
 
-    private int baseHealth;
     private String name;
+    private int baseHealth;
+    private int baseAttack;
 
     private Armor armor;
+    private Weapon weapon;
 
-    public Player(String name, int baseHealth) {
+    private int level;
+    private int incrementHealth;
+    private int incrementAttack;
+
+    public Player(String name) {
         this.name = name;
-        this.baseHealth = baseHealth;
+        this.baseHealth = 100;
+        this.baseAttack = 100;
+        this.level = 1;
+        this.incrementHealth = 20;
+        this.incrementAttack = 20;
     }
 
     public void display() {
-        System.out.println("Player : " + this.name);
-        System.out.println("Health : " + this.baseHealth);
+        System.out.println("Player\t\t: " + this.name);
+        System.out.println("Level\t\t: " + this.level);
+        System.out.println("Max Health\t: " + this.maxHealth());
+        System.out.println("Attack\t\t: " + this.getAttackPower());
     }
 
     public void setArmor(Armor armor) {
         this.armor = armor;
     }
 
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void levelUp() {
+        this.level++;
+    }
+
     public int maxHealth() {
-        return this.baseHealth + this.armor.getAddHealth();
+        return this.baseHealth + this.level * this.incrementHealth + this.armor.getAddHealth();
+    }
+
+    public int getAttackPower() {
+        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
+    }
+}
+
+class Weapon {
+    private String name;
+    private int attack;
+
+    public Weapon(String name, int attack) {
+        this.name = name;
+        this.attack = attack;
+    }
+
+    public int getAttack() {
+        return this.attack;
     }
 }
 
@@ -42,12 +80,33 @@ class Armor {
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Player player1 = new Player("Marni", 100);
+
+        // Player 1
+        Player player1 = new Player("Marni");
 
         Armor armor1 = new Armor("Baju Besi", 5, 100);
 
+        Weapon weapon1 = new Weapon("Pedang", 10);
+
         player1.setArmor(armor1);
+        player1.setWeapon(weapon1);
+
+        // Player 2
+        Player player2 = new Player("Isabella");
+
+        Armor armor2 = new Armor("Baju Koko", 2, 100);
+
+        Weapon weapon2 = new Weapon("Panah", 7);
+
+        player2.setArmor(armor2);
+        player2.setWeapon(weapon2);
 
         player1.display();
+        player1.levelUp();
+        player1.display();
+
+        player2.display();
+        player2.levelUp();
+        player2.display();
     }
 }
