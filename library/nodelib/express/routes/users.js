9ef351df0f816f50c7813a1a0058cf48ecef002e
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2025-03-13 20:52:00 +0700

    Learn Express JS

--- library/nodelib/express/routes/users.js
+++ library/nodelib/express/routes/users.js
@@ -1,13 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Users page');
-})
+router.get("/", (req, res) => {
+  res.send("Users List");
+});
 
-router.post('/new', (req, res) => {
-    res.send('Create User');
-})
+// router.get('/new', (req, res) => {
+//     res.send('New User Form');
+// })
 
+// Cannot find
+router.post("/", (req, res) => {
+  res.send("Create User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`User ID is ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User ID with ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`DeleteUser ID is ${req.params.id}`);
+  });
+
+router.get("/id", (req, res) => {
+  res.send(`User ID ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`Update User ID with ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`DeleteUser ID is ${req.params.id}`);
+});
+
+// It will overwrite this
+// router.get('/new', (req, res) => {
+//     res.send('New User Form');
+// })
 
 module.exports = router;
