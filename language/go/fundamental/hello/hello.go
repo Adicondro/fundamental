69b168f3cf9d396e1e83a09fa6008133e8fc67b0
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2025-08-10 22:51:59 +0700

    playing with golang

--- language/go/fundamental/hello/hello.go
+++ language/go/fundamental/hello/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello World")
+	// fmt.Printf("Hello i am %v", "Hello")
+
+	// var nama string = "Adicondro"
+	var nama1 = "Adicondro"
+	// fmt.Println(nama)
+	fmt.Printf("Hallo nama saya "+nama1+" Bertipe data %T \n", nama1)
+	fmt.Println(kill)
 }
